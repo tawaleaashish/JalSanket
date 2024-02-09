cf82bbd6df7f24a28cca3aaa9217b5c723619fac
Aashish Tawale <108067149+tawaleaashish@users.noreply.github.com>
2024-02-09 16:34:29 +0530

    registration link updated

--- src/JalSanket/Login/UserRegister/register.js
+++ src/JalSanket/Login/UserRegister/register.js
@@ -3,5 +3,5 @@ const registerForm = document.getElementById('registerForm');
         registerForm.addEventListener('submit', (event) => {
             event.preventDefault(); 
             alert('Registration successful!');
-            window.location.href = '../loginpage/index.html';
+            window.location.href = '../../index.html';
         });
