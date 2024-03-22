d8131b280df36966f0fcbaa2612891c3a0896740
Aashish Tawale <108067149+tawaleaashish@users.noreply.github.com>
2024-03-22 23:32:04 +0530

    login problem solved

--- src/JalSanket/index.js
+++ src/JalSanket/index.js
@@ -3,30 +3,29 @@ const userBtn = document.getElementById('userBtn');
 const loginBtn = document.getElementById('loginBtn');
 const UserloginForm = document.getElementById('UserloginForm');
 const AdminloginForm = document.getElementById('AdminloginForm');
-let i=0;
+let i = 0;
 
 userBtn.addEventListener('click', () => {
-    UserloginForm.style.display='block';
-    AdminloginForm.style.display='none';
-    i=0;
+    UserloginForm.style.display = 'block';
+    AdminloginForm.style.display = 'none';
+    i = 0;
 });
 
 adminBtn.addEventListener('click', () => {
-    UserloginForm.style.display='none';
-    AdminloginForm.style.display='block';
-    i=1;
+    UserloginForm.style.display = 'none';
+    AdminloginForm.style.display = 'block';
+    i = 1;
 });
 import supabase from "./supabaseClient.js";
 async function loginUser() {
     if (i == 0) {
-        let userloginId = document.getElementById("userloginId").value;
-        let password = document.getElementById("password").value;
-        const { data, error } = await supabase
+        let userloginId = document.getElementById("ulid").value;
+        let password = document.getElementById("passw").value;
+        let { data , error } = await supabase
             .from('UserInfo')
-            .select('*')
-            .eq('userloginId', userloginId)
-            .eq('password', password);
-
+            .select("*")
+            .eq('userloginId',userloginId)
+            .eq('password',password);            
         if (error) {
             console.error("Error fetching data from Supabase:", error.message);
             return;
@@ -40,14 +39,13 @@ async function loginUser() {
         }
     }
     if (i == 1) {
-        let ADLoginName = document.getElementById("ADLoginName").value;
-        let ADpassword = document.getElementById("ADpassword").value;
+        let ADLoginName = document.getElementById("aulid").value;
+        let ADpassword = document.getElementById("apassw").value;
         const { data, error } = await supabase
             .from('AdministratorInfo')
-            .select('*')
-            .eq('ADLoginName', ADLoginName)
-            .eq('ADpassword', ADpassword);
-        console.log(data)
+            .select("*")
+            .eq('ADLoginName',ADLoginName)
+            .eq('ADpassword',ADpassword);    
         if (error) {
             console.error("Error fetching data from Supabase:", error.message);
             return;
