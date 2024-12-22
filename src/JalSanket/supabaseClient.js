6bdecc4b95b051950a1df7d014ed9260930490d7
Aashish Tawale <108067149+tawaleaashish@users.noreply.github.com>
2024-12-22 15:02:37 +0530

    new supabase update

--- src/JalSanket/supabaseClient.js
+++ src/JalSanket/supabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js';
-const supabaseUrl = 'https://limodurjtyztcksbqspy.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxpbW9kdXJqdHl6dGNrc2Jxc3B5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY1MTgyMzMsImV4cCI6MjAyMjA5NDIzM30.xN086TIUrf87882AMnVEXPazv6fV3sfFobBetpKBM_M'
+const supabaseUrl = 'https://jteiiibyahylswlrkdaw.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0ZWlpaWJ5YWh5bHN3bHJrZGF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ4NTcxMDgsImV4cCI6MjA1MDQzMzEwOH0.Kd5oMuoeeapuaA4osQMuVIKr2R2QCVlm9YTqeFG6Wvc'
 const supabase = createClient(supabaseUrl, supabaseKey)
 export default supabase
